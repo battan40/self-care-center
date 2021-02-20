1ebf79fe9f46fd89a13e583564bde86bf91e1cc9
Angela Battillo <angela.battillo@gmail.com>
2021-02-20 13:01:08 -0700

    Add and style buttons for favorting

--- main.js
+++ main.js
@@ -2,8 +2,11 @@ var receiveMsgButton = document.querySelector('#recieve-msg');
 var meditationImage = document.querySelector('.meditate-image');
 var receiveMsgButton = document.querySelector('form')
 var randomQuote = document.querySelector('.random-phrase');
-var form = document.forms[0]; //grabbing the form in my html doc
-var radios = form.elements['message']; //create a var, go to forms, select an indicated el
+var form = document.forms[0];
+var radios = form.elements['message'];
+
+var favoriteMessages = [];
+
 receiveMsgButton.addEventListener('submit', displayRandomQuote);
 
 function displayRandomQuote(event) {
@@ -17,9 +20,6 @@ function displayRandomQuote(event) {
       meditationImage.classList.add('hidden');
       randomQuote.classList.remove('hidden');
     }
-   console.log(form, "form");
-   console.log(radios, "just radios");
-   console.log(radios.value, "radios");
 }
 
 function getRandomIndex(array) {
