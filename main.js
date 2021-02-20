227e53846a5fc67f34ef2d1cbf8c7e3fadfed97f
Angela Battillo <angela.battillo@gmail.com>
2021-02-19 19:21:35 -0700

    Add query selectors and event listeners

---
+++ main.js
@@ -0,0 +1,38 @@
+var affirmationButton = document.querySelector('#affirmation-button');
+var mantraButton = document.querySelector('#mantra-button');
+var receiveMsgButton = document.querySelector('#recieve-msg');
+var randomQuote = document.querySelector('#random-phrase');
+
+affirmationButton.addEventListener('click', getQuote);
+mantraButton.addEventListener('click', getQuote);
+receiveMsgButton.addEventListener('click', getQuote);
+
+
+
+function getQuote() {
+
+}
+
+
+
+
+
+
+if event.target.classname === 'html class name the affirmation radio button click' && the receive button click then run the randomizer function through the affiration array
+
+and hide the image (display to none)
+insert adjacent inner html into the section
+using h1 in the html
+return
+
+
+
+add html h1 target
+
+event listeners for click of both/each radio button
+
+call my data.js file in my html
+
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
+}
